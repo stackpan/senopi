857efaf336da19aecbfa9f6246f9217c21426ee5
Ivan Rizkyanto <ivanrizqy.ir@gmail.com>
2023-03-19 21:54:14 +0700

    Fix missing annotation for TaskGroupRepository

--- src/main/java/com/ivanzkyanto/stolia/repository/TaskGroupRepository.java
+++ src/main/java/com/ivanzkyanto/stolia/repository/TaskGroupRepository.java
@@ -2,6 +2,8 @@ package com.ivanzkyanto.stolia.repository;
 
 import com.ivanzkyanto.stolia.entity.TaskGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskGroupRepository extends JpaRepository<TaskGroup, String> {
 }
