001918a43f1dd3b08ca4fc4684293fbbcdbe5f3b
Ivan <ivanrizqy.ir@gmail.com>
2023-12-29 18:24:38 +0700

    Initial commit

---
+++ src/main/java/com/ivanzkyanto/senopi/SenopiApplication.java
@@ -0,0 +1,13 @@
+package com.ivanzkyanto.senopi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenopiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SenopiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ivanzkyanto/senopi/SenopiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanzkyanto.senopi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenopiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
